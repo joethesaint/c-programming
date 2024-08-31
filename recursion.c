8abaaf22a3bf6ead23b2042ef9bb2f27b85e8d43
joethesaint <joebamisaye068@gmail.com>
2024-08-31 15:24:59 +0100

    feat: Add factorial calculation using recursion in recursion.c

--- recursion.c
+++ recursion.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 
-/* 0 - 4B numbers is the max value of unsigned numbers  */
+/* Function to calculate the factorial of a number */
 unsigned int factorial(unsigned int n)
 {
+    /* Base case: if n is 0 or 1, return 1 */
     if (n <= 1)
         return 1;
+    
+    /* Recursive case: multiply n with factorial of (n-1) */
     return n * factorial(n - 1);
-} /* This fn works perfectly for calculating the factorial of both 0 and 1 */
+}
 
 int main()
 {
     unsigned int num;
+    
+    /* Prompt the user to enter a number */
     printf("Enter a number: ");
+    
+    /* Read the number from user input */
     scanf("%u", &num);
+    
+    /* Calculate and print the factorial of the entered number */
     printf("Factorial of %u is %u\n", num, factorial(num));
+    
     return 0;
 }
